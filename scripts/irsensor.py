ebd2a0dc5dff3bfd4d8acda644bbf77165e3ee8c
Harshit Sureka <hs29590@gmail.com>
2017-09-14 12:49:45 +0530

    stopping print

--- scripts/irsensor.py
+++ scripts/irsensor.py
@@ -8,13 +8,13 @@ from IR6SensorModule import IR6SensorModule
 
 def talker():
     pub = rospy.Publisher('irsensor/readings/', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+    rospy.init_node('irsensor', anonymous=True)
     rate = rospy.Rate(50) # 10hz
     irsensor = IR6SensorModule(18,23,24,25,8,7);
     while not rospy.is_shutdown():
 #        hello_str = "hello world %s" % rospy.get_time()
         sensorReadings = irsensor.readSensor();
-        rospy.loginfo(sensorReadings)
+#        rospy.loginfo(sensorReadings)
         pub.publish(sensorReadings)
         rate.sleep()
 
